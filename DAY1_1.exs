dcb202c56474a2e4cf91acf7870874215d4bd622
valtay <valtay@valtay.corp>
2025-12-10 22:21:52 +0100

    D1_1

---
+++ DAY1_1.exs
@@ -0,0 +1,18 @@
+defmodule Day1 do 	
+	def parse(line, acc) do
+		newcount = if (rem acc[:pos], 100) == 0 
+			do 
+				acc[:count] + 1
+			else
+				acc[:count] 
+			end
+		case String.trim(line) do 
+			"L" <> x -> [count: newcount, pos: acc[:pos] - String.to_integer(x)]
+			"R" <> x -> [count: newcount, pos: acc[:pos] + String.to_integer(x)]
+		end
+	end
+end
+
+File.stream!("input_1_1", [:read]) \
+|> Enum.reduce([count: 0, pos: 50], &Day1.parse/2) \
+|> IO.inspect
