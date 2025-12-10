34923e344dae468e5f80759151f372922d5ebd0c
valtay <valtay@valtay.corp>
2025-12-10 23:02:08 +0100

    autoformat

--- DAY1_1.exs
+++ DAY1_1.exs
@@ -1,19 +1,20 @@
-defmodule Day1 do 	
-	def parse(line, acc) do
-		newcount = if (rem acc[:pos], 100) == 0 
-			do 
-				acc[:count] + 1
-			else
-				acc[:count] 
-			end
-		case line do 
-			"L" <> x -> [count: newcount, pos: acc[:pos] - String.to_integer(x)]
-			"R" <> x -> [count: newcount, pos: acc[:pos] + String.to_integer(x)]
-		end
-	end
+defmodule Day1 do
+  def parse(line, acc) do
+    newcount =
+      if rem(acc[:pos], 100) == 0 do
+        acc[:count] + 1
+      else
+        acc[:count]
+      end
+
+    case line do
+      "L" <> x -> [count: newcount, pos: acc[:pos] - String.to_integer(x)]
+      "R" <> x -> [count: newcount, pos: acc[:pos] + String.to_integer(x)]
+    end
+  end
 end
 
-File.stream!("input_1_1", [:read]) \
+File.stream!("input_1_1", [:read])
 |> Enum.map(&String.trim/1)
-|> Enum.reduce([count: 0, pos: 50], &Day1.parse/2) \
-|> IO.inspect
+|> Enum.reduce([count: 0, pos: 50], &Day1.parse/2)
+|> IO.inspect()
